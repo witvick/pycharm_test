63a3bf97f24cc3ada4e580574f99448bce4549ba
witvick <nitishkumar37373@gmail.com>
2025-02-05 12:43:06 +0530

    commited on new branch change-branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
-def two(n):
-    print("hello this is main file ")
-please apply 
-n=input("enter name")
-print(n,two(n))
+def multiply(num1,num2):
+    return (num1* num2)
+n1=int(input("enter num 1"))
+n2=int(input("enter num 2"))
+res=(multiply(n1,n2))
+print(res)
